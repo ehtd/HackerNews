e6774af1c28c3a5a820d32336378f79d72073e7a
Ernesto Torres <ernesto.torres.d@gmail.com>
2014-10-22 12:50:15 -0500

    Initial commit

---
+++ Hackyto/Hackyto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackyto
+//
+//  Created by Ernesto Torres on 10/22/14.
+//  Copyright (c) 2014 ehtd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
