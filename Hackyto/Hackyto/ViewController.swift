d1e1ae9bb4a8de7573abc12865bbc9896619cca2
Ernesto Torres <ernesto.torres.d@gmail.com>
2014-10-23 16:13:45 -0500

    Download by batch

--- Hackyto/Hackyto/ViewController.swift
+++ Hackyto/Hackyto/ViewController.swift
@@ -34,12 +34,8 @@ class ViewController: UIViewController {
             
             if (self.topStories != nil && self.topStories?.count > 0){
                 
-                let firstObject: AnyObject = self.topStories!.objectAtIndex(0)
-                
-                let itemId = ("\(firstObject)")
+                self.retrieveStories(startingIndex: 0, endingIndex: 20)
 
-                self.retrieveStoryWithId(itemId)
-                
             }
             
             }, withCancelBlock: { error in
@@ -49,25 +45,41 @@ class ViewController: UIViewController {
     
     func retrieveStoryWithId(storyId: String!)
     {
-        println(storyId)
-        
         var itemURL = "https://hacker-news.firebaseio.com/v0/item/" + storyId
         var storyRef = Firebase(url:itemURL)
         
         storyRef.observeSingleEventOfType(.Value, withBlock: { snapshot in
             
             var details = snapshot.value as NSDictionary
-            println(details)
             
             let key: AnyObject? = details["id"]
 
             self.detailedStories[("\(key)")] = details
             
-            println(self.detailedStories)
+            println("Total stories: \(self.detailedStories.count)")
             
             }, withCancelBlock: { error in
                 println(error.description)
         })
+        
+    }
+    
+    func retrieveStories(startingIndex from:Int, endingIndex to:Int)
+    {
+        assert(from <= to, "From should be less than To")
+        if (self.topStories == nil)
+        {
+            return;
+        }
+        
+        for (var i = from; i < to; i++)
+        {
+            let item: AnyObject = self.topStories!.objectAtIndex(i)
+            
+            let itemId = ("\(item)")
+            
+            self.retrieveStoryWithId(itemId)
+        }
         
     }
 
