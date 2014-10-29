9835993ca51f0efdc652a884e5f1b2bc97488a19
Ernesto Torres <ernesto.torres.d@gmail.com>
2014-10-29 16:29:26 -0600

    Reenabling iPad support. Adding indicator for webView

--- Hackyto/Hackyto/Controllers/WebViewController.swift
+++ Hackyto/Hackyto/Controllers/WebViewController.swift
@@ -8,17 +8,23 @@
 
 import UIKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     var url: String?
     
+    var hud: MBProgressHUD? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.hud = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        self.hud?.mode = MBProgressHUDModeIndeterminate
         
         if let urlToLoad = url {
             var request = NSURLRequest(URL: NSURL(string: urlToLoad)!)
             webView.loadRequest(request)
+            webView.delegate = self
         }
         
     }
@@ -28,16 +34,12 @@ class WebViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-//webSegue
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+        self.hud?.hide(true)
     }
-    */
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        self.hud?.hide(true)
+    }
 
 }
