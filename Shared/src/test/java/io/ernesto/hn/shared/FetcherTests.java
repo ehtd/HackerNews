903694641e5c049b8b1e9b9a2e4e39b75c91ea48
Ernesto Torres <ernesto.torres@mutualmobile.com>
2017-08-07 14:51:14 -0500

    Update shared project with Fetcher + tests

---
+++ Shared/src/test/java/io/ernesto/hn/shared/FetcherTests.java
@@ -0,0 +1,68 @@
+package io.ernesto.hn.shared;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class FetcherTests {
+    private long maxTimeout = 10000;
+    private String baseUrl = "https://hacker-news.firebaseio.com/v0/";
+
+    @Test
+    public void testFetchingIDList() {
+        Fetcher fetcher = new Fetcher(baseUrl);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        FutureTask<String> future =
+                new FutureTask<String>(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        return fetcher.fetchURLSegment("topstories.json");
+                    }
+                });
+
+        executor.submit(future);
+
+        try {
+            String fetchedResult = future.get(maxTimeout, TimeUnit.MILLISECONDS);
+            System.out.print(fetchedResult);
+            assertTrue(fetchedResult != null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(true);
+    }
+
+    @Test
+    public void testFetchItem() {
+        Fetcher fetcher = new Fetcher(baseUrl);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        FutureTask<String> future =
+                new FutureTask<String>(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        return fetcher.fetchURLSegment("item/10483024.json");
+                    }
+                });
+
+        executor.submit(future);
+
+        try {
+            String fetchedResult = future.get(maxTimeout, TimeUnit.MILLISECONDS);
+            System.out.print(fetchedResult);
+            assertTrue(fetchedResult != null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(true);
+    }
+}
