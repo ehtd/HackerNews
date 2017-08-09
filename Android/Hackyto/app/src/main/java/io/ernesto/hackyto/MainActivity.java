6ee338df3a243b77f95b9100dac51f6674c4cc93
Ernesto Torres <ernesto.torres@mutualmobile.com>
2017-08-09 10:36:37 -0500

    Adding internet permission to Android app

--- Android/Hackyto/app/src/main/java/io/ernesto/hackyto/MainActivity.java
+++ Android/Hackyto/app/src/main/java/io/ernesto/hackyto/MainActivity.java
@@ -2,6 +2,8 @@ package io.ernesto.hackyto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -13,11 +15,14 @@ import io.ernesto.hn.Fetcher;
 public class MainActivity extends AppCompatActivity {
 
     private Fetcher fetcher;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
 
         fetcher = new Fetcher("https://hacker-news.firebaseio.com/v0/");
 
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             String fetchedResult = future.get();
+            textView.setText(fetchedResult);
             System.out.print(fetchedResult);
         }
         catch (Exception e) {
